9014dc579b491480173c27d99d31d81e3a647cb1
Febalist <febalist@gmail.com>
2016-08-14 22:45:02 +0300

    Without gitonomy/gitlib

--- src/Version.php
+++ src/Version.php
@@ -2,43 +2,115 @@
 namespace Febalist\GitVersion;
 
 use Carbon\Carbon;
-use Gitonomy\Git\Repository;
+use DateTime;
+use Jenssegers\Date\Date as JenssegersDate;
 
 class Version
 {
-    /** @var Repository */
-    protected $repository;
+    protected $base_path;
+    protected $master_branch;
+    protected $cache = [];
 
-    public function __construct()
+    public function __construct($path = null, $master = 'master')
     {
-        $path             = base_path();
-        $this->repository = new Repository($path);
-    }
-
-    protected function head()
-    {
-        return $this->repository->getHeadCommit();
-    }
-
-    public function hash($length = null)
-    {
-        if ($length) {
-            return $this->head()->getFixedShortHash($length);
+        if (!$path) {
+            if (function_exists('base_path')) {
+                $path = base_path();
+            } else {
+                $path = getcwd();
+            }
         }
-        return $this->head()->getHash();
+        $this->base_path     = realpath($path);
+        $this->master_branch = $master;
     }
 
+    /** @return string */
+    protected function exec($command)
+    {
+        $key = md5($command);
+        if (!array_has($this->cache, $key)) {
+            $value             = shell_exec("cd \"$this->base_path\" && git $command");
+            $value             = trim($value);
+            $this->cache[$key] = $value;
+        }
+        return $this->cache[$key];
+    }
+
+    /** @return string */
+    public function current($without_prefix = false)
+    {
+        $version = $this->exec("describe $this->master_branch --match=\"v*\"");
+        if ($without_prefix && starts_with($version, 'v')) {
+            $version = substr($version, 1);
+        }
+        return $version;
+    }
+
+    /** @return array */
+    protected function parts()
+    {
+        $version = $this->current(true);
+        return explode('.', $version);
+    }
+
+    /** @return integer */
+    public function part($index)
+    {
+        $part = $this->parts()[$index];
+        return intval($part);
+    }
+
+    /** @return integer */
+    public function major()
+    {
+        return $this->part(0);
+    }
+
+    /** @return integer */
+    public function minor()
+    {
+        return $this->part(1);
+    }
+
+    /** @return integer */
+    public function patch()
+    {
+        return $this->part(2);
+    }
+
+    /** @return string */
+    public function branch()
+    {
+        return $this->exec('rev-parse --abbrev-ref HEAD');
+    }
+
+    /** @return boolean */
+    public function isMaster()
+    {
+        return $this->branch() == $this->master_branch;
+    }
+
+    /** @return JenssegersDate|Carbon|DateTime */
     public function date()
     {
-        $date = $this->head()->getCommitterDate();
-        return new Carbon($date);
+        $timestamp = $this->exec('log -1 --pretty=format:%ct');
+        if (class_exists(JenssegersDate::class)) {
+            return JenssegersDate::createFromTimestamp($timestamp);
+        }
+        if (class_exists(Carbon::class)) {
+            return Carbon::createFromTimestamp($timestamp);
+        }
+        return new DateTime("@$timestamp");
     }
 
-    public function tag()
+    /** @return string */
+    public function hash($length = null)
     {
-        $references = $this->repository->getReferences();
-        $tags       = $references->resolveTags($this->head());
-        return array_first($tags);
+        $hash = $this->exec('rev-parse HEAD');
+        if ($length) {
+            return substr($hash, 0, $length);
+        }
+        return $hash;
     }
 
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,29 @@
 <?php
 
-if (!function_exists('version')) {
-    function version()
+if (!function_exists('git_version')) {
+    function git_version($path = null)
     {
-        return new Febalist\GitVersion\Version();
+        return new Febalist\GitVersion\Version($path);
+    }
+}
+
+if (!function_exists('version')) {
+    function version($without_prefix = false)
+    {
+        return git_version()->current($without_prefix);
+    }
+}
+
+if (!function_exists('version')) {
+    function version_hash($length = 10)
+    {
+        return git_version()->hash($length);
+    }
+}
+
+if (!function_exists('version')) {
+    function version_date()
+    {
+        return git_version()->date();
     }
 }
